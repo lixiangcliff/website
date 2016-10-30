d766520f02c26cf8c05daf1960d2b728ce3ddbf8
lixiangcliff <lixiang.cliff@gmail.com>
2016-10-30 14:25:54 -0700

    lesson 21

--- music/urls.py
+++ music/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import url
 from . import views
 
 
+app_name = 'music'
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
 
-    # /music/21/
+    # /music/<album_id>/
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
 ]
