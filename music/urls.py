3778fe5f38f2e79094b72eab30986d5d1798acc1
lixiangcliff <lixiang.cliff@gmail.com>
2016-12-09 23:40:24 -0800

    lesson 29

--- music/urls.py
+++ music/urls.py
@@ -5,12 +5,9 @@ from . import views
 app_name = 'music'
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
 
     # /music/<album_id>/
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
-
-    # /music/<album_id>/
-    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 
 ]
